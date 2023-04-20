dca92ae0b6029deefdacc6c6048cd2f8ce4888b8
ZX夏夜之风 <snwcreations@qq.com>
2023-04-20 11:35:30 +0000

    use playsound instead

--- src/main/java/snw/warnonmove/EventListener.java
+++ src/main/java/snw/warnonmove/EventListener.java
@@ -1,6 +1,8 @@
 package snw.warnonmove;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -15,8 +17,9 @@ public class EventListener implements Listener {
             //System.out.println("Detected helmet");
             if (s.getType() == Material.STONE_BUTTON) {
                 //System.out.println("Playing sound!");
-                event.getPlayer().getServer().dispatchCommand(event.getPlayer(), "playsound minecraft:entity.villager.ambient ambient @a ~ ~ ~");
-                //event.getPlayer().getLocation().getWorld().playSound(event.getPlayer().getLocation(), "minecraft:entity.whale.ambient", SoundCategory.MASTER, 1, 0);
+                event.getPlayer().getLocation().getWorld().playSound(event.getPlayer().getLocation(), Sound.ENTITY_VILLAGER_AMBIENT, SoundCategory.AMBIENT, 1, 1);
+                //event.getPlayer().getServer().dispatchCommand(event.getPlayer(), "playsound minecraft:entity.villager.ambient ambient @a ~ ~ ~");
+                //event.getPlayer().getLocation().getWorld().playSound(event.getPlayer().getLocation(), "minecraft:entity.whale.ambient", SoundCategory.MASTER, 1, 0); // pitch should NOT be 0
             } else {
                 //System.out.println("NOT stone button, detected " + s);
             }
